9dfd4ec68041372a2c95236f16e49b292db2ff02
jmontanos <joss92821@hotmail.com>
2021-04-30 21:49:42 -0500

    Calcula el area de un triangulo

---
+++ area_triangulo.js
@@ -0,0 +1,17 @@
+/** El área de un triángulo se describe de la siguiente manera: A = (b * h) / 2.
+ * El reto del día de hoy es escribir un programa que tome la base y la altura como parámetros y calcule el área del triángulo.
+ * 
+ * Bonus: El programa debe determinar si el triángulo es isósceles, equilátero o escaleno. 
+ */
+
+/** PENDIENTE RETO 2 */
+const areaTriguangulo = (base = 8, altura = 4) => {
+  let area = base*altura/2;
+  return console.log(`El area del triangulo es: ${area}`);
+}
+areaTriguangulo();
+areaTriguangulo(5, 4);
+/** Solucion
+ *El area del triangulo es: 16
+ *El area del triangulo es: 10
+ */
