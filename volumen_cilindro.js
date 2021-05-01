8dd3eb8cd9d24a475cfb2467b250b4fc7b9fd37d
jmontanos <joss92821@hotmail.com>
2021-05-01 12:32:02 -0500

    Calcula el volumen de un cilindro

---
+++ volumen_cilindro.js
@@ -0,0 +1,16 @@
+/**Un cilindro es un cuerpo geométrico que requiere de varias fórmulas, 
+ * aplícalas en un programa que reciba datos como su altura y radio de las bases para mostrar el resultado acotado a un decimal. */
+
+const pi = 3.141592;
+
+const calcularVolumenCilindro = (altura, radio) => {
+  const radio_al_cuadrado = radio*radio;
+  const volumen = Math.floor(pi*radio_al_cuadrado*altura);
+  return console.log(`El volumen del cilindro es ${volumen} m3`);
+}
+/*pi*(radio*radio)*altura))*/
+calcularVolumenCilindro(10,5);
+
+/**Solucion
+ *El volumen del cilindro es 785 m3
+ */
