f3a3e7084a672998c19c16403b229ee78e2dd354
jmontanos <joss92821@hotmail.com>
2021-05-07 16:02:36 -0500

    Quitando ';'

--- numero_secreto.js
+++ numero_secreto.js
@@ -1,7 +1,7 @@
 /**Este es un juego sencillo que trata de adivinar un número aleatorio, el truco está en que no sabes cuál es ese número pero en cada ingresarás un número y sabrás si este es mayor o menor al número secreto. 
  * Así que toma en cuenta estas restricciones para intentar adivinar el número y no olvides contar el número de intentos para mostrarlo una vez aciertes.*/
 
-const NUMERO_A_ENCONTRAR = Math.floor(Math.random() * (20 - 1)) + 1;;
+const NUMERO_A_ENCONTRAR = Math.floor(Math.random() * (20 - 1)) + 1;
 let respuesta = prompt('Adivina que numero es');
 let  estadoRespuesta = true;
 
