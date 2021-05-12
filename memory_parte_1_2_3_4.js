6fec8f63e3265326389c2d461e44db5d1f2e6dae
Mooenz <joss92821@hotmail.com>
2021-05-11 20:13:05 -0500

    Agregando encabezado de Memory 4

--- memory_parte_1_2_3_4.js
+++ memory_parte_1_2_3_4.js
@@ -1,6 +1,7 @@
 /**Memory_1: Crea un código que te muestre 4 caracteres entre los siguientes de forma aleatoria: "#$%()/& */
 /**Memory_2: Lleva un registro del Orden en el que aparecieron los caracteres. */
 /**Memory_3: Dale al usuario un menú para que elija en el orden correcto los caracteres.*/
+/**Memory_4: Crea el código para evaluar si el orden del usuario fue el mismo mostrado por el programa y decirle si ganó o en caso contrario si perdió */
 
 class Memory {
   constructor() {
