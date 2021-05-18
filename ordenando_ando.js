30a000b038d1b466ff43aa02928cebdee8a786cf
Mooenz <joss92821@hotmail.com>
2021-05-18 17:27:14 -0500

    Add metodo 'ordenamiento rapido'.

--- ordenando_ando.js
+++ ordenando_ando.js
@@ -1,12 +1,17 @@
+/** Ordenando ando 1:  Genera un set de 300 nombres en la siguiente página de internet http://www.generatedata.com/?lang=es y descárgalo como CSV
+ *  Ordenando ando 2: Ordena tu set de nombres con cualquier algoritmo de ordenamiento que quieras, asegúrate de imprimir cuánto tiempo se demoró tu equipo en ordenar.
+ *  Ordenando ando 3: Prueba otro algoritmo de ordenamiento con el mismo data set y compara el tiempo, comparte en el foro cuál fue tu algoritmo con mejor rendimiento y por qué
+ */
 const fs = require('fs');
+let ordenamientoRapidoMovimientos = 0;
 
-const mensaje = (movimientos, metodo) => `En ${movimientos} movimientos realizo el metodo ${metodo}`;
+const mensaje = (movimientos, metodo) => `En ${movimientos} movimientos realizo el metodo ${metodo}.`;
 
 const ordenamientoPorInsercion = (datos) => {
   console.time();
   let i, j;
   let auxiliar, actual, movimientos = 0;
-  let length = datos.length;
+  const length = datos.length;
 
   for (i = 1; i < length; i += 1) {
     actual = datos[i];
@@ -27,7 +32,7 @@ const ordenamientoDeBurbuja = (datos) => {
   console.time();
   let i;
   let auxiliar, intercambio = 0,  movimientos = 0;
-  let length = datos.length;
+  const length = datos.length;
 
   do {
     intercambio = 0;
@@ -47,9 +52,48 @@ const ordenamientoDeBurbuja = (datos) => {
   return console.log(mensaje(movimientos, 'ordenamiento de burbuja'));
 }
 
+const ordenamientoRapido = (datos) => {
+  const length = datos.length;
+  if (length <= 1) {
+    return datos;
+  }
+
+  let pivote = datos[0], izquierda = [], derecha = [];   
+
+  for (let i = 1; i < length; i++) {
+    ordenamientoRapidoMovimientos += 1; 
+    datos[i] < pivote ? izquierda.push(datos[i]) : derecha.push(datos[i]);
+  }
+  return ordenamientoRapido(izquierda).concat(pivote, ordenamientoRapido(derecha));
+}
+
+const tiempoTomaOrdenamientoRapido = (datos) => {
+  console.time();
+  ordenamientoRapido(datos);
+  console.timeEnd();
+  return console.log(mensaje(ordenamientoRapidoMovimientos,'ordenamiento rapido'));
+}
+
 const control = (datos) => {
   ordenamientoPorInsercion(datos.split('\r\n'));
   ordenamientoDeBurbuja(datos.split('\r\n'));
+  tiempoTomaOrdenamientoRapido(datos.split('\r\n'));
 }
 
 fs.readFile('registros_300.csv', 'utf-8', (err, info) => err ? console.log('error: ', err) : control(info));
+
+/**Solucion
+ * El mejor metodo es ordenamiento rapido (Quicksort), lo que basicamente hace es dividir el array en 2 dependiendo si es menor o mayor que 'pivote'.
+ * EL metodo de insercion y burbuja (insertion sort and bubble sort) son iguales en el numero de movimientos pero difieren en tiempo de ejecucion,
+ * aunque segun wikipedia esto sea al contrario.
+ * 
+ * 'El ordenamiento de burbuja es asintóticamente equivalente en tiempos de ejecución con el ordenamiento por inserción en el peor de los casos, 
+ * pero ambos algoritmos difieren principalmente en la cantidad de intercambios que son necesarios. Resultados experimentales como los descubiertos 
+ * por Astrachan han demostrado que el ordenamiento por inserción funciona considerablemente mejor incluso con listas aleatorias. Por esta razón, 
+ * muchos libros de algoritmos modernos evitan usar el ordenamiento de burbuja, reemplazándolo por el ordenamiento por inserción'.
+ * 
+ * 
+ * En 21673 movimientos realizo el metodo ordenamiento por insercion.
+ * En 21673 movimientos realizo el metodo ordenamiento de burbuja.
+ * En 2786 movimientos realizo el metodo ordenamiento rapido.
+ */
