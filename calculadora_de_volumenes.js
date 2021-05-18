322b9d5ad6261ca10ed2925ee1f0e7726f4333dd
Mooenz <joss92821@hotmail.com>
2021-05-18 11:27:15 -0500

    Quitando sentencia switch y se reemplaza por un objeto 'objetos'

--- calculadora_de_volumenes.js
+++ calculadora_de_volumenes.js
@@ -1,37 +1,31 @@
 /**Un cilindro es un cuerpo geométrico que requiere de varias fórmulas,
  *aplícalas en un programa que reciba datos como su altura y radio de las bases para mostrar el resultado acotado a un decimal. */
+const pi = 3.141592;
+let volumen = 0;
 
 const enviarMensaje = (objeto, volumen) => console.log(`El volumen del ${objeto} es ${volumen} m3`);
 
-
 const volumenes = (objetoGeometrico, altura, radio) => {
-  const pi = 3.141592;
-  const radio_al_cuadrado = radio*radio;
-  const radio_al_cubo = radio*radio*radio;
-  let volumen = 0;
+  const radio_al_cuadrado = Math.pow(radio,2);
+  const radio_al_cubo = Math.pow(radio,3);
 
-  switch (objetoGeometrico) {
-    case 'cilindro':
-      volumen = Math.floor(pi*radio_al_cuadrado*altura);
-      enviarMensaje(objetoGeometrico, volumen);
-      break;
-    case 'cubo':
-      volumen = Math.floor(altura*altura*altura);
-      enviarMensaje(objetoGeometrico, volumen);
-      break;
-    case 'esfera':
-      volumen = Math.floor((4/3)*pi*radio_al_cubo);
-      enviarMensaje(objetoGeometrico, volumen);
-      break;  
-    default: 'Puedes elejir entre cilindro, cubo, esfera';
-      break;
+  const objetos = {
+    cilindro : volumen = Math.floor(pi*radio_al_cuadrado*altura),
+    cubo : volumen = Math.floor(Math.pow(altura,3)),
+    esfera : volumen = Math.floor((4/3)*pi*radio_al_cubo)
   }
-  return
+
+  if(objetos[objetoGeometrico]) {
+    return enviarMensaje(objetoGeometrico, objetos[objetoGeometrico]);
+  } else {
+    return console.log(`Este objeto: ${objetoGeometrico}, no existe`);
+  }
 }
 
 volumenes('cilindro', 10, 5);
-volumenes('cubo', 10);
+volumenes('cubo', 10, 0);
 volumenes('esfera', 0, 8);
+volumenes('casa', 0, 8);
 
 /**Solucion
  * El volumen del cilindro es 785 m3
