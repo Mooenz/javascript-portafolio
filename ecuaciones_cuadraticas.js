cd60805beb0d8e6498a8fab693094d16f7a8be92
Mooenz <joss92821@hotmail.com>
2021-09-23 15:55:47 -0500

    nuevo ejercicio

---
+++ ecuaciones_cuadraticas.js
@@ -0,0 +1,2 @@
+// Seguro te acuerdas que en la escuela te enseñaron a resolver ecuaciones de segundo grado mediante la fórmula general para la obtención de raíces. Una ecuación de segundo grado tiene la forma: (A * X^2) + (B * X) + C = 0. Crea una calculadora que pueda resolver cualquier ecuación cuadrática.
+
