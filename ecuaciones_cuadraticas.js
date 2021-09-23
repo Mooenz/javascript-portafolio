2590ef4e28091927f7018cc98340d5629de94d3d
Mooenz <joss92821@hotmail.com>
2021-09-23 15:58:34 -0500

    Cambio

--- ecuaciones_cuadraticas.js
+++ ecuaciones_cuadraticas.js
@@ -1,2 +1,3 @@
 // Seguro te acuerdas que en la escuela te enseñaron a resolver ecuaciones de segundo grado mediante la fórmula general para la obtención de raíces. Una ecuación de segundo grado tiene la forma: (A * X^2) + (B * X) + C = 0. Crea una calculadora que pueda resolver cualquier ecuación cuadrática.
 
+console.log('Soy un cambio')
