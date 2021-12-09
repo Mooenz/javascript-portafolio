4ec26598d62be88adbc7b0e918e7a567606ad7e1
Jose Montaño <80846086+Mooenz@users.noreply.github.com>
2021-12-09 16:29:55 -0500

    delete files

--- platzi-curso-practico-js/3. Promedio, moda, mediana/promedio.js
+++
@@ -1,7 +0,0 @@
-const calcularMediaAritmetica = (lista) => {
-  let sumaLista = lista.reduce((valorAcumulado = 0, nuevoElemento) => valorAcumulado + nuevoElemento);
-  
-  const promedioLista = sumaLista / lista.length;
-
-  return promedioLista;
-}
